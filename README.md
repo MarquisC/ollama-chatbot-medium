860bcd2de11f7af3f5ca9f064a1ae87b1f00b13a
Marquis Chamberlain <marquischamberlain@gmail.com>
2024-04-28 23:10:03 -0400

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+# Ollama Chatbot Integration with Flask and Poetry
+
+
+## Prerequisites
+Before running the script, make sure you have the following installed:
+
+Python 3.x
+Poetry
+Ollama Python client library (ollama)
+Flask
+
+You can install Poetry using the following command:
+
+```bash
+curl -sSL https://install.python-poetry.org | python3 -
+```
+After installing Poetry, navigate to the project directory and run the following command to install dependencies:
+
+```bash
+poetry install
+```
+This will install Flask and the Ollama client library.
+
+## Usage
+
+
+To run the application, please execute:
+
+```bash
+poetry run python main.py
+```
+
+The application is now available at `http://127.0.0.1:5000/`
---
+++ main.py
@@ -0,0 +1,49 @@
+from ollama import Client
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+ollama_client = Client(host='http://localhost:11434')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+chat_history: list[dict] = list()
+
+
+@app.route('/process_message', methods=['POST'])
+def process_message():
+    global ollama_client
+    global chat_history
+
+    message = request.form['message']
+
+    try:
+        if len(chat_history) == 0:
+            print("Creating chat history with initial message!")
+            chat_history = [
+                {
+                    'role': 'user',
+                    'content': message,
+                },
+            ]
+        else:
+            chat_history.append({
+                'role': 'user',
+                'content': message,
+            })
+            print(f"Appending new user input to chat, history length is now [{len(chat_history)}]")
+
+        response = ollama_client.chat(model='llama2', messages=chat_history)
+        print(f"Adding LLM response {response} to chat history. History length"
+              f"is now {len(chat_history)}")
+        chat_history.append(response["message"])
+        return response["message"]["content"]
+    except Exception as e:
+        return {"Error": str(e)}
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
